fc3b79d89f71c5b3bc4d0216d3135d2952f36f04
Sebastián Vicencio <sivicencio@gmail.com>
2020-11-05 18:48:49 -0300

    update lecture 18 examples

--- clases/clase-18-client-side-javascript-ii/ejemplos/script.js
+++ clases/clase-18-client-side-javascript-ii/ejemplos/script.js
@@ -16,6 +16,41 @@ $(function() {
   });
 });
 
+function createPhotoBlock(photoData) {
+  const photoElem = document.createElement('li');
+  const photoTitle = document.createElement('h3');
+  const photoImg = document.createElement('img');
+  photoElem.setAttribute('class', 'photo');
+  photoTitle.textContent = photoData.title;
+  photoImg.src = photoData.thumbnailUrl;
+  photoElem.appendChild(photoTitle);
+  photoElem.appendChild(photoImg);
+  return photoElem;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM loaded (vanilla)');
+  const url = 'http://jsonplaceholder.typicode.com/albums/1/photos';
+  const xhrButton = document.querySelector('.ajax-xhr-btn');
+  const photosList = document.querySelector('.body ul');
+
+  xhrButton.addEventListener('click', function() {
+    const xhr = new XMLHttpRequest();
+    const xhrText = xhrButton.textContent;
+    xhr.open('GET', url);
+    xhr.onload = function() {
+      const { response } = xhr;
+      // xhr.response
+      // console.log(JSON.parse(response));
+      photosList.innerHTML = '';
+      JSON.parse(response)
+        .map(createPhotoBlock)
+        .forEach((elem) => photosList.appendChild(elem));
+      xhrButton.textContent = xhrText;
+      xhrButton.disabled = false;
+    };
+    xhrButton.textContent = 'Cargando...';
+    xhrButton.disabled = true;
+    xhr.send();
+  });
 });
